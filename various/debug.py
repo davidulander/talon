686d579c0dfa9bbb48598f3624c0b1fd48a07046
Christian Hultin <christian.h.hultin@gmail.com>
2019-12-18 11:38:00 +0100

    update

---
+++ various/debug.py
@@ -0,0 +1,11 @@
+# from talon.engine import engine
+
+## Enable this script to be able to see the output in the Talon Log
+
+# def listener(topic, m):
+#     if topic == 'cmd' and m['cmd']['cmd'] == 'g.load' and m['success'] == True:
+#         print('[grammar reloaded]')
+#     else:
+#         print(topic, m)
+
+# engine.register('', listener)
