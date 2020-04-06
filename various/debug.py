49f044ce84096bf69d5be2e00a51bf2ea3d58517
David Ulander <david.ulander@netlight.com>
2020-04-06 20:36:56 +0200

    disable log

--- various/debug.py
+++ various/debug.py
@@ -1,11 +1,11 @@
-from talon.engine import engine
+# from talon.engine import engine
 
-# Enable this script to be able to see the output in the Talon Log
+# # Enable this script to be able to see the output in the Talon Log
 
-def listener(topic, m):
-    if topic == 'cmd' and m['cmd']['cmd'] == 'g.load' and m['success'] == True:
-        print('[grammar reloaded]')
-    else:
-        print(topic, m)
+# def listener(topic, m):
+#     if topic == 'cmd' and m['cmd']['cmd'] == 'g.load' and m['success'] == True:
+#         print('[grammar reloaded]')
+#     else:
+#         print(topic, m)
 
-engine.register('', listener)
+# engine.register('', listener)
